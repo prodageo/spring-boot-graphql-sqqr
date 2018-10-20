508a744d95312791e3fe6001504b73914bf43f93
Frédéric BAUCHER <fred@baucher.net>
2018-10-20 17:04:29 +0200

    Update ApplicationWebConfig.java

--- src/main/java/com/igu/developer/demo/ApplicationWebConfig.java
+++ src/main/java/com/igu/developer/demo/ApplicationWebConfig.java
@@ -1,5 +1,7 @@
 package com.igu.developer.demo;
 
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +20,7 @@ public class ApplicationWebConfig implements WebMvcConfigurer {
       InternalResourceViewResolver bean = new InternalResourceViewResolver();
  
       bean.setViewClass(JstlView.class);
+       // src/main/webapp/WEB-INF/view/
       bean.setPrefix("/WEB-INF/view/");
       bean.setSuffix(".jsp");
  
